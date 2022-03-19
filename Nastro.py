4bfc7437717b674ec99a49584cfb96b53bab6769
Harvey Barnes <ldshadowlord@gmail.com>
2022-03-19 22:14:05 +0000

    Added initial parsers

--- Nastro.py
+++ Nastro.py
@@ -1,9 +1,43 @@
-
-
-
 if __name__ == "__main__":
     #Run This Code if Being Run From CLI
-    pass
+    import argparse
+
+    #Do Argument Parsing
+    parser = argparse.ArgumentParser(description="Perform Admin Commands against the Application", prog="Nastro-Admin")
+
+    parserGroup1 = parser.add_mutually_exclusive_group(required=True)
+    parserGroup1.add_argument("-create_new_db",
+                              help="Create a fresh database at the current version",
+                              action="store_true")
+    parserGroup1.add_argument("-upgrade_schema",
+                              help="Update an older database to the current version",
+                              action="store_true")
+    parserGroup1.add_argument("-delete_db",
+                              help="Factory Reset the database to the current version",
+                              action="store_true")
+    parserGroup1.add_argument("-dump_db",
+                              help="Dump Database to File",
+                              type=argparse.FileType('w', encoding='latin-1'),
+                              action="store")
+    parserGroup1.add_argument("-import_db",
+                              help="Import Database from File",
+                              type=open,
+                              action="store")
+    parserGroup1.add_argument("-db_version", "-db",
+                              help="Get the version that the database is running",
+                              action="store_true")
+    parserGroup1.add_argument("-version", "-v",
+                              help="Get the version that the application is running",
+                              action="version",
+                              version="%(prog)s 0.1")
+    
+    parser.add_argument("Config Path",
+                        help="The path to the config file required by the application (Usually config.json)",
+                        type=open,
+                        action="store")
+
+    args = parser.parse_args()
+    #End Argument Parsing
 
 else:
     #Run This Code if Being Imported
---
+++ tape.py
@@ -0,0 +1,63 @@
+if __name__ == "__main__":
+    #Run This Code if Being Run From CLI
+    import argparse
+
+    #Do Argument Parsing
+    parser = argparse.ArgumentParser(description="Perform Tape Commands against the Application", prog="Nastro-Tape")
+    
+    exclusiveGroup = parser.add_mutually_exclusive_group()
+    exclusiveGroup.add_argument("-delete_tape", "-delete",
+                              help="Delete a Tape from the DB",
+                              action="store_true")
+    exclusiveGroup.add_argument("-move_tape", "-move",
+                              help="Move a Tape between Locations",
+                              action="store_true")
+    exclusiveGroup.add_argument("-load_tape", "-load",
+                              help="Load a Tape into a Library",
+                              action="store_true")
+    exclusiveGroup.add_argument("-unload_tape", "-unload",
+                              help="Unload a Tape from a Library",
+                              action="store_true")
+    exclusiveGroup.add_argument("-new_tape", "-new",
+                              help="Add a new Tape to the DB",
+                              action="store_true")
+    exclusiveGroup.add_argument("-edit_tape", "-edit",
+                              help="Edit a Tape in the DB",
+                              action="store_true")
+
+    parser.add_argument("-destroy", "-de",
+                        help="Add if you want the tape to be marked as destroyed.",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("-scratch", "-sc",
+                        help="Add if you want the tape to be marked as scratch.",
+                        action=argparse.BooleanOptionalAction)
+    parser.add_argument("-location", "-lo",
+                        help="The 6 character location ID",
+                        action="store")
+    parser.add_argument("-type", "-ty",
+                        help="The tape type (ex: LTO6/SDLT 4)",
+                        action="store")
+    parser.add_argument("-software", "-sw",
+                        help="The backup software type (ex: LTFS/Netbackup 7.1.7)",
+                        action="store")
+    parser.add_argument("-library", "-li",
+                        help="The library that the tape is being placed in",
+                        action="store")
+    parser.add_argument("-slot", "-sl",
+                        help="The slot that the tape is being moved to",
+                        action="store")
+    
+    parser.add_argument("Tape Barcode",
+                        help="The 8 character barcode used to identify the tape",
+                        action="store")    
+    parser.add_argument("Config Path",
+                        help="The path to the config file required by the application (Usually config.json)",
+                        type=open,
+                        action="store")
+
+    args = parser.parse_args()
+    #End Argument Parsing
+
+else:
+    #Run This Code if Being Imported
+    pass
